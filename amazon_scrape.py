2fd266bdb6f8d9379cae677183e9f0bc64210876
saikiranreddygangidi <40819884+saikiranreddygangidi@users.noreply.github.com>
2020-08-14 18:22:24 +0530

    update

--- amazon_scrape.py
+++ amazon_scrape.py
@@ -4,13 +4,12 @@ import json
 import time
 
 
-
-
 def get_data(next_page):
     all_product_details=[]
     try:
         while next_page != None:
             page=requests.get(next_page)
+            #it wait for 2 second complete page is loaded 
             time.sleep(2)
             soup =BeautifulSoup(page.content,'html.parser')
             all_title=soup.find_all("span",{"class":"a-size-medium a-color-base a-text-normal"})
@@ -24,16 +23,17 @@ def get_data(next_page):
                 next_page=next_page.find('a')
                 if next_page is not None:
                     next_page="https://www.amazon.in"+next_page["href"]
-            print(next_page)
+            
     except Exception as e:
         print(e)
     return all_product_details
 
-
+#start executing from here
 if __name__ == "__main__": 
     PRODUCT=input("enter product name you want:- ")
     next_page="https://www.amazon.in/s?k="+PRODUCT   
     all_product_details=get_data(next_page)
+    PRODUCT=PRODUCT.replace(" ","_")
     all_product_details=json.dumps(all_product_details)   
     file = open('data_'+PRODUCT+'.json', 'w') 
     file.write(all_product_details) 
